a86e29f09ec418f548199bfe6965e26f092f2180
ilyaDevops <ilya.gasan2020@mail.ru>
2022-12-29 03:39:18 +0300

    main mechan no func main

--- main.cpp
+++ main.cpp
@@ -21,7 +21,17 @@ public:
 
 
     Entity(Image &image, float X, float Y, int W, int H, std::string Name){
-
+        x = X; y = Y; //координата появления спрайта
+        w = W/2; h = H/2;
+        name = Name;
+        moveTimer = 0;
+        dx = 0; dy = 0;
+        speed = 0;
+        CurrentFrame = 0;
+        health = 100;
+        life = true; //инициализировали логическую переменную жизни, герой жив
+        texture.loadFromImage(image); //заносим наше изображение в текстуру
+        sprite.setTexture(texture); //заливаем спрайт текстуро
 
     }
 
@@ -43,18 +53,112 @@ class Player :public Entity {
 public:
     int playerScore;//эта переменная может быть только у игрока
     Player(Image &image, float X, float Y, int W, int H, std::string Name) :Entity(image, X,Y, W, H, Name){
-
+        playerScore = 0;
+        state = stay;
+        if (name == "Player1"){
+            //Задаем спрайту один прямоугольник для
+            //вывода одного игрока. IntRect – для приведения типов
+            sprite.setTextureRect(IntRect(0, 0, w, h));
+        }
     }
     void control(){
-
+        if (Keyboard::isKeyPressed(Keyboard::Left)) {
+                   state = left;
+                   speed = 0.1;
+               }
+       if (Keyboard::isKeyPressed(Keyboard::Right)) {
+           state = right;
+           speed = 0.1;
+       }
+       if (Keyboard::isKeyPressed(Keyboard::Up)) {
+           state = up;
+           speed = 0.1;
+       }
+       if (Keyboard::isKeyPressed(Keyboard::Down)) {
+           state = down;
+           speed = 0.1;
+       }
     }
 //Метод проверки столкновений с элементами карты
     void checkCollisionWithMap(float Dx, float Dy) {
-
+        for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты
+                    for (int j = x / 32; j<(x + w) / 32; j++)
+                    {
+                        if (TileMap[i][j] == '0')//если элемент тайлик земли
+                        {
+                            if (Dy > 0) { y = i * 32 - h; dy = 0; }//по Y
+                            if (Dy < 0) { y = i * 32 + 32; dy = 0; }//столкновение с верхними краями
+                            if (Dx > 0) { x = j * 32 - w; dx = 0; }//с правым краем карты
+                            if (Dx < 0) { x = j * 32 + 32; dx = 0; }// с левым краем карты
+                        }
+                        if (TileMap[i][j] == 's') {
+                            playerScore++; //если взяли камень
+                            TileMap[i][j] = ' ';
+                        }
+                        if (TileMap[i][j] == 'f') {
+                            health -= 40;//если взяли ядовитый цветок
+                            TileMap[i][j] = ' ';//убрали цветок
+                        }
+                        if (TileMap[i][j] == 'h') {
+                            health += 20;//если взяли сердечко
+                            TileMap[i][j] = ' ';//убрали сердечко
+                        }
+                    }
     }
+
     void update(float time) //метод "оживления/обновления" объекта класса.
     {
+        if (life) {//проверяем, жив ли герой
+                    control();//функция управления персонажем
+                    switch (state)//делаются различные действия в зависимости от состояния
+                    {
+                        case right:{//состояние идти вправо
+                            dx = speed;
+                            CurrentFrame += 0.005*time;
+                            if (CurrentFrame > 10) CurrentFrame -= 10;
+                                sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
+                                sprite.setScale({1,1});
+                            break;
 
+                        }
+                        case left:{//состояние идти влево
+                            dx = -speed;
+                            CurrentFrame += 0.005*time;
+
+                            if (CurrentFrame > 10) CurrentFrame -= 10;
+                                sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
+
+                            break;
+                        }
+                        case up:{//идти вверх
+                            dy = -speed;
+                            CurrentFrame += 0.005*time;
+                            if (CurrentFrame > 10) CurrentFrame -= 10;
+                            sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
+                            break;
+                        }
+                        case down:{//идти вниз
+                            dy = speed;
+                            CurrentFrame += 0.005*time;
+                            if (CurrentFrame > 10) CurrentFrame -= 10;
+                            sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
+                            break;
+                        }
+                        case stay:{//стоим
+                            dy = speed;
+                            dx = speed;
+                            break;
+                        }
+                    }
+                    x += dx*time; //движение по “X”
+                    checkCollisionWithMap(dx, 0);//обрабатываем столкновение по Х
+                    y += dy*time; //движение по “Y”
+                    checkCollisionWithMap(0, dy);//обрабатываем столкновение по Y
+                    speed = 0; //обнуляем скорость, чтобы персонаж остановился.
+                    //state = stay; //состояние - стоим
+                    sprite.setPosition(x, y); //спрайт в позиции (x, y).
+                    if (health <= 0){ life = false; }//если жизней меньше 0, либо равно 0, то умираем
+                }
     }
 };
 
@@ -66,15 +170,86 @@ public:
     int direction;//направление движения врага
 
     Enemy(Image &image, float X, float Y, int W, int H, std::string Name) :Entity(image, X, Y, W, H, Name){
-
+        if (name == "EasyEnemy"){
+                //Задаем спрайту один прямоугольник для
+                //вывода одного игрока. IntRect – для приведения типов
+                sprite.setTextureRect(IntRect(0, 0, w, h));
+                direction = rand() % (3); //Направление движения врага задаём случайным образом
+                //через генератор случайных чисел
+                speed = 0.1;//даем скорость.этот объект всегда двигается
+                dx = speed;
+                sprite.setScale({1,1});
+                }
     }
     void checkCollisionWithMap(float Dx, float Dy)//ф-ция проверки столкновений с картой
     {
-
+        for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты
+                   for (int j = x / 32; j<(x + w) / 32; j++)
+                   {
+                       if (TileMap[i][j] == '0')//если элемент - тайлик земли
+                       {
+                           if (Dy > 0) {
+                               y = i * 32 - h; dy = -0.1;
+                               direction = rand() % (3); //Направление движения врага
+                           }//по Y
+                           if (Dy < 0) {
+                               y = i * 32 + 32; dy = 0.1;
+                               direction = rand() % (3);//Направление движения врага
+                           }//столкновение с верхними краями
+                           if (Dx > 0) {
+                               x = j * 32 - w; dx = -0.1;
+                               direction = rand() % (3);//Направление движения врага
+                           }//с правым краем карты
+                           if (Dx < 0) {
+                               x = j * 32 + 32; dx = 0.1;
+                               direction = rand() % (3); //Направление движения врага
+                           }// с левым краем карты
+                       }
+                   }
     }
     void update(float time)
     {
-
+        if (name == "EasyEnemy"){//для персонажа с таким именем логика будет такой
+                    if (life) {//проверяем, жив ли герой
+                    switch (direction)//делаются различные действия в зависимости от состояния
+                    {
+                        case 0:{//состояние идти вправо
+                            dx = speed;
+                            CurrentFrame += 0.005*time;
+                            if (CurrentFrame > 3) CurrentFrame -= 3;
+                            sprite.setTextureRect(IntRect(32 * int(CurrentFrame), 0, 32, 32));
+                            break;
+                        }
+                        case 1:{//состояние идти влево
+                            dx = -speed;
+                            CurrentFrame += 0.005*time;
+                            if (CurrentFrame > 3) CurrentFrame -= 3;
+                            sprite.setTextureRect(IntRect(32 * int(CurrentFrame), 32, 32, 32));
+                            break;
+                        }
+                        case 2:{//идти вверх
+                            dy = -speed;
+                            CurrentFrame += 0.005*time;
+                            if (CurrentFrame > 3) CurrentFrame -= 3;
+                            sprite.setTextureRect(IntRect(32 * int(CurrentFrame), 64, 32, 32));
+                            break;
+                        }
+                        case 3:{//идти вниз
+                            dy = speed;
+                            CurrentFrame += 0.005*time;
+                            if (CurrentFrame > 3) CurrentFrame -= 3;
+                            sprite.setTextureRect(IntRect(32 * int(CurrentFrame), 96, 32, 32));
+                            break;
+                        }
+                    }
+                    x += dx*time; //движение по “X”
+                    checkCollisionWithMap(dx, 0);//обрабатываем столкновение по Х
+                    y += dy*time; //движение по “Y”
+                    checkCollisionWithMap(0, dy);//обрабатываем столкновение по Y
+                    sprite.setPosition(x, y); //спрайт в позиции (x, y).
+                    if (health <= 0){ life = false; }//если жизней меньше 0, либо равно 0, то умираем
+                    }
+                }
     }
 };//класс Enemy закрыт
 
