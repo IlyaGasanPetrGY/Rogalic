d0dc6868775c43dae795920bb7925043e610f697
Vadim <mitske2461@gmail.com>
2022-12-29 05:04:28 +0300

    added drawing map

--- main.cpp
+++ main.cpp
@@ -318,7 +318,21 @@ int main()
 
     while (window.isOpen())
         {
+        for (int i = 0; i < HEIGHT_MAP; i++)
+            for (int j = 0; j < WIDTH_MAP; j++)
+            {
+                if (TileMap[i][j] == ' ') s_map.setTextureRect(IntRect(0, 0, 32, 32));
+                window.draw(s_map);
+                if (TileMap[i][j] == 's') s_map.setTextureRect(IntRect(32, 0, 32, 32));
+                if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));
+                if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32));//цветок
 
+                if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(96, 0, 32, 32));
+
+                //сердце
+                s_map.setPosition(j * 32, i * 32);
+                window.draw(s_map);
+            }
          }
 
     return 0;
